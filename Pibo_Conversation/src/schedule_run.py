2d2740f5b68f8a3d21f21209400a6b7c01a3bd5f
daaa02 <dyk98498@gmail.com>
2023-12-08 16:20:23 +0900

    Update_1208

--- Pibo_Conversation/src/schedule_run.py
+++ Pibo_Conversation/src/schedule_run.py
@@ -6,14 +6,26 @@ import csv
 import time
 from datetime import datetime
 
+from openpibo.audio import Audio
+from openpibo.motion import Motion
+from openpibo.oled import Oled
+
+
 sys.path.append('/home/pi')
 from schedule_update import UpdateSchedule
 from Pibo_Package_07.Pibo_Conversation.data.text_to_speech import TextToSpeech, text_to_speech
 from Pibo_Package_07.Pibo_Conversation.src.drive_upload import drive_upload
 
-from openpibo.audio import Audio
-from openpibo.motion import Motion
-from openpibo.oled import Oled
+# sys.path.append('/home/pi/Pibo_Package_07/Pibo_Conversation/')
+from Pibo_Package_07.Pibo_Conversation.data.c_conversation_manage import ConversationManage, WordManage
+from Pibo_Package_07.Pibo_Conversation.data.text_to_speech import TextToSpeech, text_to_speech
+import Pibo_Package_07.Pibo_Conversation.data.behavior.behavior_list as behavior
+
+
+cm = ConversationManage()
+wm = WordManage()
+audio = TextToSpeech()
+
 
 us = UpdateSchedule()
 pibo_audio = Audio()
@@ -49,79 +61,30 @@ class RunSchedule():
         self.completion = len(data2)
         print('완료한 활동 개수:', self.completion)
         
-
-        ## 고정된 스케줄로 진행(~3일차)
-                
-        # 최초 실행시 자기소개 시나리오 실행
-        if self.completion == 0:
-            self.act = f'Pibo_Conversation/src/greeting.py'
         
-        if 1 <= self.completion <= 8:
-            fix = {1:'Pibo_Conversation/src/Fairytale/19_shepherd.py', 2:'Pibo_Conversation/src/Roleplay/02_strong.py',
-                   3:'Pibo_Conversation/src/Solution/01_badword.py', 4:'Pibo_Play/src/Com/com_4.py', 5:'Pibo_Play/src/Soc/soc_6.py',
-                   6:'Pibo_Conversation/src/Etiquette/03_cough.py', 7:'Pibo_Play/src/Cog/cog_1.py', 8:'Pibo_Play/src/Mus/mus_11.py'}   
-            self.act = fix.get(self.completion)
-            
+        cm.tts(bhv="do_question_S", string=f"활동 이름을 말씀해 주세요.")
+        answer = cm.responses_proc(re_bhv="do_waiting_A", re_q="활동 이름을 말씀해 주세요.")
+        print(answer)
         
-        ## 선호도 계산해서 활동 스케줄 결정(4일차~)
+        # 키워드 단순화
         
-        if 9 <= self.completion <= 10 or self.completion >= 12:
-            for i in range(0, len(data2)):
-                for j in range(0, 4):
-                    data2[i][j] = float(data2[i][j])    # 점수 값이 string 형태로 들어있어서 flaot로 변환
-            print(data2)
-
-            for i in range(0, len(data2)-1):            # 각 항목끼리 모두 합연산
-                data3 = [sum(i) for i in zip(*data2)]   # data1 목록 만큼
-            print("선호도 총합:", data3)   
-            
-            result = us.update(new_preference=data3)[0]    
-                    
-            
-            if result == '사회성':            
-                # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화
-                if self.completion % 2 == 0: 
-                    rand = random.choice([2, 3])
-                    self.act = f'Pibo_Play/src/Soc/soc_{rand}.py'
-                
-                if self.completion % 2 != 0:
-                    rand = random.choice(['06_street', '14_giveaway'])
-                    self.act = f'Pibo_Conversation/src/Etiquette/{rand}.py'
-                    # self.act = f'Pibo_Conversation/src/Etiquette/00_qrcode.py'
-                
-            if result == '의사소통':
-                # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화
-                if self.completion % 2 == 0: 
-                    rand = random.choice([1, 2])
-                    self.act = f'Pibo_Play/src/Com/com_{rand}.py'
-                
-                if self.completion % 2 != 0:
-                    rand = random.choice(['02_salt', '18_rabbit'])
-                    self.act = f'Pibo_Conversation/src/Fairytale/{rand}.py'
-                
-            if result == '인지':
-                # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화
-                if self.completion % 2 == 0: 
-                    rand = random.choice([3, 10])
-                    self.act = f'Pibo_Play/src/Cog/cog_{rand}.py'
-                
-                if self.completion % 2 != 0:
-                    rand = random.choice(['02_wash', '03_night', '17_thank', '18_cheerup'])
-                    self.act = f'Pibo_Conversation/src/Solution/{rand}.py'
-                
-            if result == '근육':
-                # 완료한 활동 개수가 짝수면 놀이, 홀수면 대화
-                if self.completion % 2 == 0:
-                    rand = random.choice([1, 8, 12])
-                    self.act = f'Pibo_Play/src/Mus/mus_{rand}.py'
-                    
-                if self.completion % 2 != 0:
-                    rand = random.choice(['03_tiny', '04_flying'])
-                    self.act = f'Pibo_Conversation/src/Roleplay/{rand}.py'
+        # 1. 대화-자기소개
+        if "소개" in answer[0][1] or "만나" in answer[0][1]:
+            self.act = 'Pibo_Conversation/src/greeting.py'
         
-        # 마지막 활동은 헤어짐 시나리오 실행 
-        if self.completion == 11:
-            self.act = f'Pibo_Conversation/src/goodbye.py'              
+        # 2. 대화-QR코드()
+        if "카드" in answer[0][1] or "코드" in answer[0][1] or "예절" in answer[0][1]:
+           self.act = 'Pibo_Conversation/src/Etiquette/03_cough.py'
+        
+        # 3. 놀이-춤추는유령
+        if "춤" in answer[0][1] or "유령" in answer[0][1]:
+            self.act = 'Pibo_Play/src/Com/com_4.py'
+        
+        # 4. 놀이-휴지길
+        if "휴지" in answer[0][1] or "길" in answer[0][1]:
+            self.act = 'Pibo_Play/src/Cog/cog_1.py'
+        
+        
         
         try:
             folder = "/home/pi/UserData"
--- Pibo_Conversation/src/start_touch.py
+++ Pibo_Conversation/src/start_touch.py
@@ -55,53 +55,7 @@ while True:
                 device.eye_off(); time.sleep(0.5); 
                 device.eye_on(255,255,255); time.sleep(0.5) 
                 
-            cm.tts(bhv="do_question_S", string=f"활동 이름을 말씀해 주세요.")
-            answer = cm.responses_proc(re_bhv="do_waiting_A", re_q="활동 이름을 말씀해 주세요.")
-            print(answer)
+            break
             
-            # 키워드 단순화
-            # 1. 대화-자기소개
-            if "소개" in answer[0][1] or "만나" in answer[0][1]:
-                os.system('python3 /home/pi/Pibo_Package_07/Pibo_Conversation/src/greeting.py')
-                break
-            
-            # 2. 대화-QR코드()
-            if "카드" in answer[0][1] or "코드" in answer[0][1] or "예절" in answer[0][1]:
-                cm.tts(bhv="do_question_L", string=f"잠시만 기다려줘.")
-                os.system('python3 /home/pi/Pibo_Package_07/Pibo_Conversation/src/Etiquette/03_cough.py') 
-                break
-            
-            # 3. 놀이-춤추는유령
-            if "춤" in answer[0][1] or "유령" in answer[0][1]:
-                cm.tts(bhv="do_question_L", string=f"잠시만 기다려줘.")
-                os.system('python3 /home/pi/Pibo_Package_07/Pibo_Play/src/Com/com_4.py') 
-                break
-            
-            # 4. 놀이-휴지길
-            if "휴지" in answer[0][1] or "길" in answer[0][1]:
-                cm.tts(bhv="do_question_L", string=f"잠시만 기다려줘.")
-                os.system('python3 /home/pi/Pibo_Package_07/Pibo_Play/src/Cog/cog_1.py') 
-                break
-            
-            else:
-                oled.clear()
-                continue
-            
-            # else:    
-            #     cm.tts(bhv="do_question_L", string=f"새로운 활동을 시작해볼까?")
-            #     answer = cm.responses_proc(re_bhv="do_question_L", re_q="오늘의 활동을 시작해볼까?",
-            #                             pos_bhv="do_stop", pos=f"시작하자~", 
-            #                             neg_bhv="do_stop", neg=f"알겠어. 나랑 놀고 싶다면 다시 쓰다듬어줘")
-                
-            #     time.sleep(1)
-            #     if answer[0][0] == 'positive' or answer[0][0] == 'action':
-            #         cm.tts(bhv="do_stop", string=f"잠시만 기다려줘. 재미있는 활동을 생각해볼께!")
-            #         break
-                    
-            #     else:
-            #         oled.clear()
-            #         continue
-            
-            
-# os.system('python3 /home/pi/Pibo_Package_07/Pibo_Conversation/src/schedule_run.py')
+os.system('python3 /home/pi/Pibo_Package_07/Pibo_Conversation/src/schedule_run.py')
     
